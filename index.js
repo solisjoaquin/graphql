280868dd021e89d2ef974936dd87320dc55d8250
Joaquin <sol19008@byui.edu>
2020-11-12 17:21:13 -0300

    adding graphql files

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express();
 const port = process.env.port || 3000
 
-const schema = buildSchema(`
-    type Query {
-        hello: String
-       
-    }
-`)
+const schema = buildSchema(
+    readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+)
 
 // configurar los resolvers
 
-const resolvers = {
-    hello: () => {
-        return 'Hola mundo'
-    }
-
-}
 
 
 app.use('/api', graphqlHTTP({
